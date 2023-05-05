ed53eaeffe806af0345c647b45c42b9a7cc9378b
Enl-Z <EZhao1997@outlook.com>
2023-05-05 10:20:25 +0800

    Deployed 4bd3bca with MkDocs version: 1.4.2

--- assets/javascripts/extra.js
+++ assets/javascripts/extra.js
@@ -2,6 +2,7 @@ const updateScheme = e => {
     var giscus = document.querySelector(".giscus-frame");
     var a = localStorage.getItem('data-md-color-scheme');
     var theme = a === "default" ? "light" : "dark";
+    // alert(a + " -> " + theme);
     giscus.contentWindow.postMessage(
         { giscus: { setConfig: { theme } } },
         "https://giscus.app"
@@ -15,6 +16,7 @@ const updateBoxFontColor = e => {
     if (a !== "default") {
         var elements = document.getElementsByClassName('box');
         for (var i in elements) {
+            // alert(elements[i].style.color);
             elements[i].style.color = "white";
         }
     }
@@ -27,9 +29,11 @@ const updateBoxFontColor = e => {
     }
     var a = localStorage.getItem('data-md-color-scheme');
     if (a == null) {
+        // alert("未设置主题");
         a = "slate";
         localStorage.setItem("data-md-color-scheme", a);
     }
     document.body.setAttribute('data-md-color-scheme', a);
+    // updateScheme();
     updateBoxFontColor();
 })()
